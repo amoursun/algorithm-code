43d4274e8d92961506fd9d000a48761b273eb4ca
ganyanlin <ganyanlin@baidu.com>
2025-04-01 15:13:29 +0800

    [BPIT-HCM-92153] 算法

--- 有效括号.js
+++ 有效括号.js
@@ -31,3 +31,45 @@ var generateParenthesis = function(n) {
 
     return Array.from(set);
 };
+
+/**
+ * 
+ * "([)]"	false	交叉嵌套无效 
+ * "{[]}"	true	正确嵌套 
+ * "]()["	false	首字符为右括号
+ */
+function isValidBrackets(value) {
+    // 空字符串是有效的
+    if (!value) {
+        return true;
+    }
+    // 长度为奇数，直接返回 false
+    if (value.length % 2 !== 0) {
+        return false;
+    }
+    // 如果第一个字符是右括号，直接返回 false
+    if ([')', '}', ']'].includes(value[0])) {
+        return false;
+    }
+    const stack = [];
+    const map = {'(': ')', '[': ']', '{': '}'}; // 括号映射表
+    
+    for (let i = 0; i < value.length; i++) {
+        const val = value[i];
+        if (map[val]) {
+            stack.push(val);
+        }
+        else {
+            if (stack.length === 0) {
+                // 栈空说明右括号无匹配
+                return false;
+            };
+            const top = stack.pop();
+            if (map[top] !== val) {
+                // 栈顶元素和当前元素不匹配
+                return false;
+            }
+        }
+    }
+    return stack.length === 0; // 栈空说明括号匹配
+}
